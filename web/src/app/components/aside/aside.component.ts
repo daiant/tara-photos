97a500e6275ce6d06bac023fcf4b8afb9f08e567
daiant <cmg2512@gmail.com>
2024-01-03 19:51:46 +0100

    a

--- web/src/app/components/aside/aside.component.ts
+++ web/src/app/components/aside/aside.component.ts
@@ -1,13 +1,30 @@
 import { CommonModule } from "@angular/common";
-import { Component } from "@angular/core";
+import { Component, inject } from "@angular/core";
+import { NavigationStart, Router, RouterLink } from "@angular/router";
 
 @Component({
   selector: "tara-aside",
   templateUrl: "./aside.component.html",
   styleUrl: "./aside.component.css",
   standalone: true,
-  imports: [CommonModule]
+  imports: [CommonModule, RouterLink]
 })
 export class AsideComponent {
-
+  isCurrentLink(link: string): boolean {
+    return link === this.pathname;
+  }
+  router = inject(Router);
+  pathname?: string;
+  ngOnInit(): void {
+    this.router.events.subscribe((val) => {
+      if (val instanceof NavigationStart) {
+        this._checkUrl(val.url);
+      }
+    });
+    this._checkUrl(this.router.url);
+  }
+  _checkUrl(url: string) {
+    const pathname = new URL("http://test.com" + url).pathname;
+    this.pathname = pathname;
+  }
 }
--- web/src/app/components/upload/upload.component.ts
+++ web/src/app/components/upload/upload.component.ts
@@ -11,13 +11,16 @@ import { FileService } from "../../../lib/files/file.service";
   imports: [CommonModule]
 })
 export class UploadComponent {
+  triggerSubmit(event: Event) {
+    const form = (event.target as HTMLInputElement).parentElement as HTMLFormElement;
+    this.handleSubmit(form);
+  }
   files: Array<FileType> = [];
   fileService = inject(FileService);
 
-  async handleSubmit(event: SubmitEvent) {
-    event.preventDefault();
-    const form = new FormData(event.target as HTMLFormElement);
-    await this.fileService.uploadFiles(form);
-    (event.target as HTMLFormElement).reset();
+  async handleSubmit(form: HTMLFormElement) {
+    const formData = new FormData(form);
+    await this.fileService.uploadFiles(formData);
+    form.reset();
   }
 }
