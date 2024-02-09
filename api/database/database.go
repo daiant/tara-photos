418fe12ac6a494c9e16fe33753c6a2f04fc47a52
daiant <cmg2512@gmail.com>
2024-02-09 23:34:17 +0100

    A

--- api/database/database.go
+++ api/database/database.go
@@ -2,6 +2,7 @@ package database
 
 import (
 	"database/sql"
+	"fmt"
 	"log"
 
 	_ "github.com/go-sql-driver/mysql"
@@ -11,7 +12,9 @@ func Connect() *sql.DB {
 	// Configure the database connection (always check errors)
 	// Local: 127.0.0.1:3306
 	// Docker (mysql)
-	db, err := sql.Open("mysql", "root:files@(127.0.0.1:3306)/files?parseTime=true")
+  // db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	dsn := fmt.Sprintf("%s:%s@(%s:%s)/%s", "files", "files", "localhost", "3306", "files")  + "?charset=utf8mb4&parseTime=True&loc=Local"
+	db, err := sql.Open("mysql", dsn)
 	if err != nil {
 		log.Fatal("Error openning db connection: ", err)
 	}
@@ -19,7 +22,7 @@ func Connect() *sql.DB {
 	// Make sure to check the error.
 	pingErr := db.Ping()
 	if pingErr != nil {
-		log.Fatal("Error connecting to db: ", err)
+		log.Fatal("Error connecting to db: ", pingErr)
 	}
 	return db
 }
