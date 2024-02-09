5bf4252bf3494162149aa50a306054bec28ba071
daiant <cmg2512@gmail.com>
2024-02-09 23:57:35 +0100

    feat: login :)

--- web/src/app/views/login/login.component.ts
+++ web/src/app/views/login/login.component.ts
@@ -2,25 +2,43 @@ import { Component, inject } from "@angular/core";
 import { Router } from "@angular/router";
 import { UserService } from "../../../lib/users/user.service";
 import { TokenService } from "../../../lib/token/token.service";
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: "tara-login",
-  templateUrl: "./login.component.html"
+  templateUrl: "./login.component.html",
+  styleUrl: './login.component.css',
+  standalone: true,
+  imports: [CommonModule],
 })
 export class LoginComponent {
   tokenService = inject(TokenService)
   userService = inject(UserService)
   router = inject(Router);
+  loading = false;
+  error?: string;
   handleSubmit(event: SubmitEvent) {
-    event.preventDefault()
+    event.preventDefault();
+    this.loading = true;
     const formData = new FormData(event.target as HTMLFormElement);
     this.userService.login(formData).then(data => {
-      if (!data) return;
+      this.loading = false;
+      if (!data) {
+        this._showError();
+        return;
+      };
+      this.error = undefined;
       this.tokenService.persistToken(data);
       this.userService.getUserInfo().then(data => {
         this.tokenService.persistItem("user", data)
       });
-      this.router.navigate(["/"])
+      this.router.navigate(["/"]);
+    }).catch(error => {
+      this.loading = false;
+      this._showError();
     });
+  }
+  _showError() {
+    this.error = 'Ha ocurrido un error.'
   }
 }
